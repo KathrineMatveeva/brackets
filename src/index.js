0512a8f1f3061e4b55a4b95bfca1e180d677fde2
Kate <kate.matveeva@gmail.com>
2021-03-13 17:12:38 +0300

    feat: add implementation for check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairs = new Map(bracketsConfig);
+    let stack = [];
+    for (let b of str.split('')) {
+        if (pairs.get(stack[stack.length - 1]) === b) {
+            stack.pop()
+        } else if (pairs.has(b)) {
+            stack.push(b)
+        } else return false
+    }
+    return stack.length === 0
 }
